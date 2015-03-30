1f79b5ae8ebe02add2f8eafc94099a9f80c69372
Philipp Acsany <filip.axani@gmail.com>
2015-03-30 10:20:50 +0200

    Add comments for script workflow

---
+++ dropToInDesign/dropToInDesign.py
@@ -0,0 +1,10 @@
+
+# Receive one or more fonts.
+
+# Copy every font into the document-fonts folder of a predefined directory
+
+# Read font data
+
+# Add all data to template InDesign file. (Create a page for every font.)
+
+# Open InDesign
