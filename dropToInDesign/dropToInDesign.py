b2de67bb3b98581eaaf10b62568302f8e49ac2c6
Philipp Acsany <filip.axani@gmail.com>
2015-03-30 10:29:56 +0200

    Add class structure

--- dropToInDesign/dropToInDesign.py
+++ dropToInDesign/dropToInDesign.py
@@ -1,10 +1,19 @@
 
-# Receive one or more fonts.
+def getInputpaths():
+    return []
 
-# Copy every font into the document-fonts folder of a predefined directory
+class CreateInDesignSpecimen:
+    def __init__(self, path_list):
+        print path_list
+        # Receive one or more fonts.
 
-# Read font data
+        # Copy every font into the document-fonts folder of a predefined directory
 
-# Add all data to template InDesign file. (Create a page for every font.)
+        # Read font data
 
-# Open InDesign
+        # Add all data to template InDesign file. (Create a page for every font.)
+
+
+if __name__ == '__main__':
+    input_paths = getInputpaths()
+    specimen = CreateInDesignSpecimen(input_paths)
