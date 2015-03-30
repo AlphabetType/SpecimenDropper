262e3a003dbae9f8fe9506c0872762c114ad1eea
Philipp Acsany <filip.axani@gmail.com>
2015-03-30 11:01:51 +0200

    Filter files for allowed filetypes

--- dropToInDesign/dropToInDesign.py
+++ dropToInDesign/dropToInDesign.py
@@ -1,5 +1,10 @@
 import subprocess
-import sys
+import sys, os
+
+class ConfigData:
+    def __init__(self):
+        # Allowed filetypes must start with a dot
+        self.allowedFiletypes = ['.otf', '.ttf']
 
 def getInputpaths():
     arguments = []
@@ -10,16 +15,31 @@ def getInputpaths():
 
 class CreateInDesignSpecimen:
     def __init__(self, path_list):
-        print path_list
         self.path = False # set to false for dev reasons
 
+        # Get config data
+        self.c = ConfigData()
+
         # Receive one or more fonts.
+        fontpath_list = self.acceptOnlyAllowedFiletypesInList(path_list)
+        print fontpath_list
 
         # Copy every font into the document-fonts folder of a predefined directory
 
         # Read font data
 
         # Add all data to template InDesign file. (Create a page for every font.)
+
+    def acceptOnlyAllowedFiletypesInList(self, pathlist_in):
+        pathlist_out = []
+        for p in pathlist_in:
+            if os.path.splitext(p)[1] in self.c.allowedFiletypes:
+                pathlist_out.append(p)
+            else:
+                print p, 'ignored, because it has no an allowed extension.'
+
+        return pathlist_out
+
 
 
 if __name__ == '__main__':
