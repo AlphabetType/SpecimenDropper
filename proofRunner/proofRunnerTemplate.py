4c54010ae5c48d459270cddc12c641e3cfba73d9
Philipp Acsany <filip.axani@gmail.com>
2015-05-11 14:14:28 +0200

    Add opentype to ff statements

--- proofRunner/proofRunnerTemplate.py
+++ proofRunner/proofRunnerTemplate.py
@@ -7,5 +7,6 @@ ff_statements = {
     'eot': '<style>@font-face { font-family: "{{basename}}"; src: url("{{filepath_long}}"); src: url("{{filepath_long}}?#iefix") format("embedded-opentype"); font-weight: normal; font-style: normal}</style>',
     'woff': '<style>@font-face { font-family: "{{basename}}"; src: url("{{filepath_long}}") format("woff"); font-weight: normal; font-style: normal}</style>',
     'ttf': '<style>@font-face { font-family: "{{basename}}"; src: url("{{filepath_long}}") format("truetype"); font-weight: normal; font-style: normal}</style>',
+    'otf': '<style>@font-face { font-family: "{{basename}}"; src: url("{{filepath_long}}") format("opentype"); font-weight: normal; font-style: normal}</style>',
     'svg': '<style>@font-face { font-family: "{{basename}}"; src: url("{{filepath_long}}#{{basename}}") format("svg"); font-weight: normal; font-style: normal}</style>',
 }
