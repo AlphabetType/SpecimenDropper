dd67062645c14666526ee3f668f02a29cdf41fef
Philipp Acsany <filip.axani@gmail.com>
2015-05-11 14:34:31 +0200

    Add method to return all found files

--- proofRunner/proofRunner.py
+++ proofRunner/proofRunner.py
@@ -17,17 +17,23 @@ def getInputpaths(debug=False):
 class ProofRunner(object):
     def __init__(self, rootPath):
         self.rootPath = rootPath
+        self.foundFiles = []
 
         for extension in template.ff_statements:
             self.createProof(extension)
 
     def createProof(self, filetype):
+        # The proof files will be created in the root of the given path.
+        # If you want to create them elsewhere, change this variable accordingly.
         proof_path = os.path.join(self.rootPath, 'proof_' + filetype + '.html')
         try:
             with open(proof_path, 'w+') as proof_file:
-                print 'Creating proof for %s files:\t %s' % (filetype, proof_path)
+                print 'Writing proof for %s files:\t %s' % (filetype, proof_path)
         except:
             print 'Error: Unable to create proof:\t %s' % proof_path
+
+    def getAllFilePaths(self):
+        return self.foundFiles
 
 
 
@@ -35,4 +41,5 @@ if __name__ == '__main__':
     input_paths = getInputpaths(debug=True)
     if input_paths:
         for path in input_paths:
-            ProofRunner(path)
+            pr = ProofRunner(path)
+            print pr.getAllFilePaths()
