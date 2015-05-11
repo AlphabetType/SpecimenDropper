5fd9244cad3e5193ccf7b94a244d0caec4dad6bf
Philipp Acsany <filip.axani@gmail.com>
2015-05-11 14:10:17 +0200

    Add debug option to input paths

--- proofRunner/proofRunner.py
+++ proofRunner/proofRunner.py
@@ -1,7 +1,12 @@
+import os
 import sys
 
-def getInputpaths():
-    if len(sys.argv[1:]) > 0:
+def getInputpaths(debug=False):
+    if debug:
+        # If you use windows, you should use a windows compatible path
+        return os.path.join(os.path.expanduser('~'), 'Desktop')
+
+    elif len(sys.argv[1:]) > 0:
         return sys.argv[1:]
     else:
         print 'Please add a path as argument when running the script.'
@@ -9,6 +14,6 @@ def getInputpaths():
 
 
 if __name__ == '__main__':
-    input_paths = getInputpaths()
+    input_paths = getInputpaths(debug=True)
     if input_paths:
-        pass
+        print os.listdir(input_paths)
